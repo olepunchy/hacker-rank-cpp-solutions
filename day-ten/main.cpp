4116b2f487e849ef7b13fe2390a5570443f9f5c1
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-09-12 16:02:45 -0500

    Adding day ten

--- day-ten/main.cpp
+++ day-ten/main.cpp
@@ -1,45 +1,4 @@
 // Day 10: Binary Numbers
-//
-// Problem:
-//
-//        Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
-//
-//Task
-//        Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
-//
-//Example
-//
-//        The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
-//
-//Input Format
-//
-//A single integer, .
-//
-//Constraints
-//
-//        Output Format
-//
-//        Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
-//
-//Sample Input 1
-//
-//5
-//Sample Output 1
-//
-//1
-//Sample Input 2
-//
-//13
-//Sample Output 2
-//
-//2
-//Explanation
-//
-//        Sample Case 1:
-//The binary representation of  is , so the maximum number of consecutive 's is .
-//
-//Sample Case 2:
-//The binary representation of  is , so the maximum number of consecutive 's is .
 
 #include <iostream>
 #include <string>
@@ -73,17 +32,6 @@ int main() {
     }
 
     cout << max;
-
-//    string binaryResult = bitset<32>(n).to_string();
-//
-//    cout << binaryResult << "\n";
-//
-//    int onesCount = count(begin(binaryResult), end(binaryResult), '1');
-//
-//    int result = (n % 2 != 0) ? onesCount - 1 : onesCount;
-//    cout << result << "\n";
-
-
 
     return 0;
 }
