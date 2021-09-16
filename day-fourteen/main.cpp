982d20c987abf37f1d72ef9797083cb2d256a8e3
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-09-16 18:09:00 -0500

    Adding unfinished solution

---
+++ day-fourteen/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+
+    public:
+        Difference(vector<int> &elements): elements(elements) {};
+        void computeDifference() {
+
+        };
+
+        int maximumDifference;
+
+};
+
+int main() {
+    // Do something
+    return 0;
+}
