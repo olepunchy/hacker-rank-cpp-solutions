3cdc694ee7c432090194f15f3f469ab4ee164477
olepunchy <olepunchy@gmail.com>
2021-09-16 19:33:20 -0500

    Completed Day 14 Solution

--- day-fourteen/main.cpp
+++ day-fourteen/main.cpp
@@ -10,8 +10,12 @@ class Difference {
 
     public:
         Difference(vector<int> &elements): elements(elements) {};
-        void computeDifference() {
 
+        void computeDifference() {
+            int smallest = *min_element(begin(elements), end(elements));
+            int largest = *max_element(begin(elements), end(elements));
+
+            maximumDifference = abs(largest - smallest);
         };
 
         int maximumDifference;
@@ -19,6 +23,28 @@ class Difference {
 };
 
 int main() {
-    // Do something
+
+    int numberOfElements = 0;
+
+    cout << "Enter the number of elements: ";
+    cin >> numberOfElements;
+
+    vector<int> theElements;
+    cout << "Enter " << numberOfElements << " numbers separated by a space: ";
+    int element;
+
+    while (cin >> element) {
+        theElements.push_back(element);
+        if (cin.get() == '\n') {
+            break;
+        }
+    }
+
+    Difference difference = Difference(theElements);
+
+    difference.computeDifference();
+
+    cout << "The difference is " << difference.maximumDifference << "\n";
+
     return 0;
 }
